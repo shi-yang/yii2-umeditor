b5e59e245adf83e4caf8742218c257f06cbb3ce7
shi-yang <dr@shiyang.me>
2016-02-01 09:39:52 +0800

    Fixed #1:改为默认的配置文件

--- assets/umeditor.config.js
+++ assets/umeditor.config.js
@@ -244,47 +244,6 @@
         //,topOffset:30
 
         //填写过滤规则
-        ,filterRules: {
-            //纯文本粘贴模式下的过滤规则
-            'filterTxtRules' : function() {
-                function transP(node) {
-                    node.tagName = 'p';
-                    node.setStyle();
-                }
-                return {
-                    //直接删除及其字节点内容
-                    '-': 'script style object iframe embed input select',
-                    'p': {
-                        $: {}
-                    },
-                    'br': {
-                        $: {}
-                    },
-                    'div': {
-                        '$': {}
-                    },
-                    'li': {
-                        '$': {}
-                    },
-                    'caption': transP,
-                    'th': transP,
-                    'tr': transP,
-                    'h1': transP,
-                    'h2': transP,
-                    'h3': transP,
-                    'h4': transP,
-                    'h5': transP,
-                    'h6': transP,
-                    'td': function(node) {
-                        //没有内容的td直接删掉
-                        var txt = !! node.innerText();
-                        if (txt) {
-                            node.parentNode.insertAfter(UE.uNode.createText('    '), node);
-                        }
-                        node.parentNode.removeChild(node, node.innerText())
-                    }
-                }
-            }()
-        }
+        //,filterRules: {}
     };
 })();
